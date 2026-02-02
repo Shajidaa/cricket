fd4af5486e5901f887be0460b8edbadf74f44c72
Shajidaa <shajidaakterlopa101@gmail.com>
2026-02-02 07:38:36 +0600

    update the video page

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -6,17 +6,20 @@ import { VideoItem } from "@/types";
 
 
 export default async function NewsPage() {
-  const { cricket_videos } = await getAllVideos();
-  console.log(cricket_videos);
+  const data = await getAllVideos();
+    console.log("Full API response:", data);
 
   return (
-    <div className="container mx-auto py-10 px-4">
-      <h1 className="text-4xl font-black italic mb-8 border-l-8 border-red-600 pl-4 ">
-        LATEST VIDEOS
-      </h1>
-
+    <>
+      
+ <div className="mb-5 bg-gradient-to-r from-black via-red-950 to-red-600 text-white p-6 shadow-lg">
+           <h1 className="text-4xl md:text-5xl text-center font-black italic mb-8 pl-4 uppercase">
+               Latest Videos
+           </h1>
+         </div>
+         <div className="container mx-auto py-10 px-4">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {cricket_videos?.map((video: VideoItem) => (
+        {data?.map((video: VideoItem) => (
           <div key={video.id} className="bg-white rounded-lg shadow-md overflow-hidden">
             <div className="relative h-48">
               <iframe
@@ -34,5 +37,6 @@ export default async function NewsPage() {
         ))}
       </div>
     </div>
+    </>
   );
 }
